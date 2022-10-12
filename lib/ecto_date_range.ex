f709789df78b1ccc9a4c2d98c9dde6022d415118
Dave Lucia <davelucianyc@gmail.com>
2022-10-12 12:21:50 -0400

    First pass implementation

--- lib/ecto_date_range.ex
+++ lib/ecto_date_range.ex
@@ -5,49 +5,81 @@ defmodule Ecto.DateRange do
 
   use Ecto.Type
 
-  defstruct [:range, :lower_inclusive, :upper_inclusive]
-
-  @type t :: %__MODULE__{
-          range: Date.Range.t(),
-          lower_inclusive: boolean(),
-          upper_inclusive: boolean()
-        }
-
   @impl Ecto.Type
   def type, do: :daterange
 
   @impl Ecto.Type
-  def cast(term) do
-    dbg()
-    :error
+  def cast(%Date.Range{} = range) do
+    {:ok, to_postgrex_range(range)}
   end
 
   @impl Ecto.Type
   def dump(%Date.Range{} = range) do
-    dbg()
+    {:ok, to_postgrex_range(range)}
+  end
 
-    {:ok,
-     %Postgrex.Range{
-       upper: range.last,
-       lower: range.first,
-       upper_inclusive: false,
-       lower_inclusive: true
-     }}
+  def dump(%Postgrex.Range{} = range) do
+    {:ok, range}
   end
 
   @impl Ecto.Type
-  def equal?(one, two) do
-    dbg()
-    true
+  def load(%Postgrex.Range{} = range) do
+    {:ok, normalize_range(range)}
   end
 
-  @impl Ecto.Type
-  def load(%Postgrex.Range{lower: lower, upper: upper}) do
-    dbg()
+  @doc """
+  Converts `Date.Range.t()` into a `Postgrex.Range.t()`
 
-    with {:ok, lower} <- Ecto.Type.load(:date, lower),
-         {:ok, upper} <- Ecto.Type.load(:date, upper) do
-      {:ok, Date.range(lower, upper)}
+      iex> Ecto.DateRange.to_postgrex_range(Date.range(~D[1989-09-22], ~D[2021-03-01]))
+      %Postgrex.Range{lower: ~D[1989-09-22], upper: ~D[2021-03-01], lower_inclusive: true, upper_inclusive: true}
+  """
+  @spec to_postgrex_range(Date.Range.t() | Postgrex.Range.t()) :: Postgrex.Range.t()
+  def to_postgrex_range(%Postgrex.Range{} = range), do: range
+
+  def to_postgrex_range(%Date.Range{first: first, last: last}) do
+    %Postgrex.Range{
+      lower: first,
+      lower_inclusive: true,
+      upper: last,
+      upper_inclusive: true
+    }
+  end
+
+  @doc """
+  Converts a Postgrex.Range.t() into a normalized form. For bounded ranges,
+  it will make the lower and upper bounds inclusive to align with the semantics
+  of Date.Range.t()
+
+      iex> range = %Postgrex.Range{lower: ~D[1989-09-22], upper: ~D[2021-03-02], lower_inclusive: true, upper_inclusive: false}
+      iex> Ecto.DateRange.normalize_range(range)
+      %Postgrex.Range{lower: ~D[1989-09-22], upper: ~D[2021-03-01], lower_inclusive: true, upper_inclusive: true}
+
+      iex> range = %Postgrex.Range{lower: ~D[1989-09-21], upper: ~D[2021-03-01], lower_inclusive: false, upper_inclusive: true}
+      iex> Ecto.DateRange.normalize_range(range)
+      %Postgrex.Range{lower: ~D[1989-09-22], upper: ~D[2021-03-01], lower_inclusive: true, upper_inclusive: true}
+
+  """
+  def normalize_range(%Postgrex.Range{upper: %Date{}, lower: %Date{}} = range) do
+    range
+    |> normalize_upper()
+    |> normalize_lower()
+  end
+
+  def normalize_range(%Postgrex.Range{} = range), do: range
+
+  defp normalize_upper(%Postgrex.Range{} = range) do
+    if range.upper_inclusive do
+      range
+    else
+      %{range | upper_inclusive: true, upper: Date.add(range.upper, -1)}
+    end
+  end
+
+  defp normalize_lower(%Postgrex.Range{} = range) do
+    if range.lower_inclusive do
+      range
+    else
+      %{range | lower_inclusive: true, lower: Date.add(range.lower, 1)}
     end
   end
 end
--- test/support/test_app/table.ex
+++ test/support/test_app/table.ex
@@ -2,8 +2,16 @@ defmodule TestApp.Table do
   @moduledoc false
   use Ecto.Schema
 
+  import Ecto.Changeset
+
   schema "my_table" do
     field(:name, :string)
     field(:foo, Ecto.DateRange)
+  end
+
+  def changeset(table, params) do
+    table
+    |> cast(params, [:name, :foo])
+    |> validate_required([:name, :foo])
   end
 end
