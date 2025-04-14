1cca2e8c9340fa5e370438b22d83770de841ff60
Dave Lucia <davelucianyc@gmail.com>
2025-04-14 10:00:32 -0400

    change to usec, always expect inclusive

--- lib/ectorange/timestamp.ex
+++ lib/ectorange/timestamp.ex
@@ -5,7 +5,7 @@ defmodule EctoRange.Timestamp do
       iex> range = {~N[2020-10-31 09:30:00], ~N[2020-11-02 10:00:00]}
       iex> cs = TestApp.Table.changeset(%TestApp.Table{name: "EctoRange.Timestamp"}, %{tsrange: range})
       iex> cs.changes
-      %{tsrange: %Postgrex.Range{lower: ~N[2020-10-31 09:30:00], upper: ~N[2020-11-02 10:00:00], lower_inclusive: true, upper_inclusive: true}}
+      %{tsrange: %Postgrex.Range{lower: ~N[2020-10-31 09:30:00.000000], upper: ~N[2020-11-02 10:00:00.000000], lower_inclusive: true, upper_inclusive: true}}
 
   `tsrange` in Postgres is a continuous range, and does not have any equivalent Elixir struct.
 
@@ -51,8 +51,7 @@ defmodule EctoRange.Timestamp do
 
   @impl Ecto.Type
   def load(%Postgrex.Range{} = range) do
-    dbg(range)
-    {:ok, normalize_range(range)}
+    {:ok, range}
   end
 
   @doc """
@@ -74,49 +73,11 @@ defmodule EctoRange.Timestamp do
     }
   end
 
-  @doc """
-  Converts a Postgrex.Range.t() into a normalized form. For bounded ranges,
-  it will make the lower and upper bounds inclusive to align with the semantics
-  of Date.Range.t()
-
-      iex> range = %Postgrex.Range{lower: ~D[1989-09-22], upper: ~D[2021-03-02], lower_inclusive: true, upper_inclusive: false}
-      iex> EctoRange.Date.normalize_range(range)
-      %Postgrex.Range{lower: ~D[1989-09-22], upper: ~D[2021-03-01], lower_inclusive: true, upper_inclusive: true}
-
-      iex> range = %Postgrex.Range{lower: ~D[1989-09-21], upper: ~D[2021-03-01], lower_inclusive: false, upper_inclusive: true}
-      iex> EctoRange.Date.normalize_range(range)
-      %Postgrex.Range{lower: ~D[1989-09-22], upper: ~D[2021-03-01], lower_inclusive: true, upper_inclusive: true}
-
-  """
-  def normalize_range(%Postgrex.Range{upper: %NaiveDateTime{}, lower: %NaiveDateTime{}} = range) do
-    range
-    |> normalize_upper()
-    |> normalize_lower()
-  end
-
-  def normalize_range(%Postgrex.Range{} = range), do: range
-
-  defp normalize_upper(%Postgrex.Range{} = range) do
-    if range.upper_inclusive do
-      range
-    else
-      %{range | upper_inclusive: true, upper: Date.add(range.upper, -1)}
-    end
-  end
-
-  defp normalize_lower(%Postgrex.Range{} = range) do
-    if range.lower_inclusive do
-      range
-    else
-      %{range | lower_inclusive: true, lower: Date.add(range.lower, 1)}
-    end
-  end
-
   defp cast_timestamp(d) do
     case d do
       nil -> {:ok, nil}
       "" -> {:ok, nil}
-      other -> Ecto.Type.cast(:naive_datetime, other)
+      other -> Ecto.Type.cast(:naive_datetime_usec, other)
     end
   end
 end
